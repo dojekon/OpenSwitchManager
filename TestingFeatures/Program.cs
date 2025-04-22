e6c0bcf8a4d766b6acc14d3bef9f2f6bca8672da
Alexander <dojekon@Mail.ru>
2025-04-22 18:30:52 +0300

    Created test console

---
+++ TestingFeatures/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var dlinkCLient = new SwitchClient.DLink("192.168.3.93", "trueadm", "{rb,thufkfrnbrf}");
+
+var authTask = await dlinkCLient.Auth();
+
+if (authTask) {
+    Console.WriteLine("Good");
+} else Console.WriteLine("Bad");
